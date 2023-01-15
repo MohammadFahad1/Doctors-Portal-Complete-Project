204e0f783e7355fcd75d7af2056a8d18d59b8441
MohammadFahad1 <fahad4bangladesh@gmail.com>
2023-01-16 00:11:11 +0600

    Router Setup Done & Navigation Bar Added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routes/Routes';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <RouterProvider router={router}>
+
+    </RouterProvider>
   );
 }
 
